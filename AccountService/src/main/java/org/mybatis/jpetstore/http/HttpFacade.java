e38210e3b0a3ba2e2c5ccb958c497d8f52f239d9
hyubuki <suhyuk.ts@gmail.com>
2025-01-24 17:43:55 +0900

    feat: Account -> Catalog Http 통신 구현 완료

--- AccountService/src/main/java/org/mybatis/jpetstore/http/HttpFacade.java
+++ AccountService/src/main/java/org/mybatis/jpetstore/http/HttpFacade.java
@@ -2,11 +2,13 @@ package org.mybatis.jpetstore.http;
 
 import org.mybatis.jpetstore.domain.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
+
 
 @Service
 public class HttpFacade {
@@ -17,8 +19,11 @@ public class HttpFacade {
 
     public List<Product> getProductListByCategory(String categoryId) {
         String url = CATALOG_SERVICE_URL + "/get/productList?catalogId=" + categoryId;
-        Product[] respList = restTemplate.postForObject(url, null, Product[].class);
-        List<Product> productList = Arrays.asList(respList);
+
+        ResponseEntity<Product[]> responseEntity = restTemplate.postForEntity(url, null, Product[].class);
+        Product[] responses = responseEntity.getBody();
+        List<Product> productList = Arrays.asList(responses);
+
         return productList;
     }
 }
