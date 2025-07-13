ea8d3a1870e1007bfdc053acc380ff365445448f
hyubuki <suhyuk.ts@gmail.com>
2025-07-13 19:38:33 +0900

    Feat: AutoConfiguration 기능을 사용해서 Redis Session 설정 자동화

--- CommonLibrary/src/main/java/org/mybatis/jpetstore/config/RedisConfig.java
+++ CommonLibrary/src/main/java/org/mybatis/jpetstore/config/RedisConfig.java
@@ -1,37 +1,28 @@
-package org.mybatis.jpetstore.session;
+package org.mybatis.jpetstore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
-@EnableRedisHttpSession
-public class SessionConfig {
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory factory = new LettuceConnectionFactory("localhost", 6379);
-        return factory;
-    }
+public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
     }
 
     @Bean
     public CookieSerializer cookieSerializer() {
         DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
-
         // 쿠키 설정
         cookieSerializer.setCookiePath("/");
         cookieSerializer.setCookieName("SESSION");
