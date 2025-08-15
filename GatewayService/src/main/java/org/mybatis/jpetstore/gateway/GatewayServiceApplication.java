05e6159ac18c996d04665ec553679a5777611525
hyubuki <suhyuk.ts@gmail.com>
2025-08-15 22:50:00 +0900

    Feat: Spring Cloud Gateway, Service Discovery 추가

--- GatewayService/src/main/java/org/mybatis/jpetstore/gateway/GatewayServiceApplication.java
+++ GatewayService/src/main/java/org/mybatis/jpetstore/gateway/GatewayServiceApplication.java
@@ -1,13 +1,13 @@
-package org.mybatis.jpetstore.order;
+package org.mybatis.jpetstore.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-public class OrderServiceApplication {
+@EnableDiscoveryClient
+public class GatewayServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderServiceApplication.class, args);
+        SpringApplication.run(GatewayServiceApplication.class, args);
     }
 }
